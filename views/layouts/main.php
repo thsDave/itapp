a502ae26d6235c4748b645fba882bd0f339ac4d2
David Ramos <david.ramos@cristosal.org>
2026-04-21 09:41:26 -0600

    Se agregó módulo de control de roles

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -60,7 +60,8 @@
   </footer>
 </div>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap4.min.js"></script>
